5c1979f1fc3fefcae417ebac87576033b593e98f
Blake DeGraw <bbdegraw@gmail.com>
2021-07-12 16:43:35 -0700

    fetching api data from ssgoh and passing to the front end successfully when called

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
+
 const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+
+app.get('/', (req, res) => {
+    axios.get('https://swgoh.gg/api/ships')
+    .then(response => res.send(response.data))
+})
 
 app.listen(8000)
